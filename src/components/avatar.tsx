2163a90580e32f6f11e834512adc9470c12c4796
gabxyz <gabxyzdev@gmail.com>
2023-02-16 17:38:30 -0300

    add `avatar.tsx` component using radix primitive

---
+++ src/components/avatar.tsx
@@ -0,0 +1,24 @@
+import * as AvatarPrimitive from "@radix-ui/react-avatar"
+
+interface AvatarProps {
+  name: string
+  imageUrl: string
+}
+
+const Avatar = ({ name, imageUrl }: AvatarProps) => (
+  <AvatarPrimitive.Root className="inline-flex h-[28px] w-[28px] select-none items-center justify-center overflow-hidden rounded-full bg-blackA-3 align-middle">
+    <AvatarPrimitive.Image
+      className="h-full w-full rounded-[inherit] object-cover"
+      src={imageUrl}
+      alt={name}
+    />
+    <AvatarPrimitive.Fallback
+      className="leading-1 flex h-full w-full items-center justify-center bg-white text-[15px] font-medium text-gray-11"
+      delayMs={600}
+    >
+      CT
+    </AvatarPrimitive.Fallback>
+  </AvatarPrimitive.Root>
+)
+
+export default Avatar
