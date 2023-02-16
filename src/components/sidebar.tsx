b81950a84959f7218198c0c36e6b091e14cd1c94
gabxyz <gabxyzdev@gmail.com>
2023-02-16 17:55:08 -0300

    refactor `sidebar.tsx` component to render user avatar if authenticated

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -1,7 +1,12 @@
+"use client"
 import { FolderHeart, Rocket, Scroll, User } from "lucide-react"
 import Link from "next/link"
+import { useSession } from "next-auth/react"
+
+import Avatar from "./avatar"
 
 const Sidebar = () => {
+  const { data: session } = useSession()
   return (
     <div className="sticky left-0 min-h-screen w-20 border-r border-gray-6 bg-gray-3">
       <div className="flex h-full flex-col items-center justify-between pb-6 pt-4 text-gray-11">
@@ -19,7 +24,18 @@ const Sidebar = () => {
             <Scroll size={20} />
           </li>
           <li>
-            <User size={20} />
+            {session?.user ? (
+              <Link href="/profile">
+                <Avatar
+                  name={session.user.name!}
+                  imageUrl={session.user.image!}
+                />
+              </Link>
+            ) : (
+              <Link href="/login">
+                <User size={20} />
+              </Link>
+            )}
           </li>
         </ul>
       </div>
