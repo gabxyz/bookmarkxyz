cbecb280f6f71e2f2a2b39bca1948197d9600567
gabxyz <gabxyzdev@gmail.com>
2023-03-03 11:05:34 -0300

    refactor login and profile pages to server components
    `app/login/page.tsx` is turned into server component and render the SingIn button client component.
    
    `app/profile/page.tsx` is also turned into a server component, it calls `getUser` to fetch user data passing the user.id from the authenticated user and renders a ProfileForm with the data.

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,27 +1,36 @@
-"use client"
-
+import { User } from "@prisma/client"
 import { redirect } from "next/navigation"
-import { signOut, useSession } from "next-auth/react"
 
-export default function Profile() {
-  const { data, status } = useSession()
+import { SignOut } from "@/components/auth-button"
+import ProfileForm from "@/components/profile-form"
+import { db } from "@/lib/db"
+import { getCurrentUser } from "@/lib/session"
 
-  if (status === "unauthenticated") {
+async function getUser(userId: User["id"]) {
+  return await db.user.findUnique({
+    where: {
+      id: userId,
+    },
+  })
+}
+
+export default async function Profile() {
+  const user = await getCurrentUser()
+
+  if (!user) {
     redirect("/login")
   }
 
+  const userData = await getUser(user.id)
+
   return (
-    <div className="flex h-full flex-col items-center justify-center gap-10 p-10">
-      <div className="flex flex-col gap-8">
+    <div className="flex h-full flex-col gap-10 p-10">
+      <div className="flex flex-col items-center justify-center gap-8">
         <p className="text-center font-medium opacity-80">
-          Logged in as <strong>{data?.user.username}</strong>
+          Logged in as <strong>{userData?.username}</strong>
         </p>
-        <button
-          onClick={() => signOut({ callbackUrl: "/" })}
-          className="w-full rounded-lg bg-gray-5 py-2 text-center text-sm font-medium text-gray-12 shadow-md hover:opacity-80 motion-safe:duration-150 motion-safe:ease-productive-standard"
-        >
-          Sign out
-        </button>
+        <ProfileForm {...userData!} />
+        <SignOut />
       </div>
     </div>
   )
