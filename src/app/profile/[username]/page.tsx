aae74acc006bfc7e03ef395eaf9b6d04942ab126
gabxyz <gabxyzdev@gmail.com>
2023-03-05 10:00:34 -0300

    refactor to fetch social links fields and make everything look nicer

--- src/app/profile/[username]/page.tsx
+++ src/app/profile/[username]/page.tsx
@@ -1,4 +1,6 @@
 import { User } from "@prisma/client"
+import clsx from "clsx"
+import { Github, Twitter } from "lucide-react"
 import { notFound } from "next/navigation"
 
 import Avatar from "@/components/avatar"
@@ -18,6 +20,8 @@ async function getUserData(username: User["username"]) {
       username: true,
       bio: true,
       image: true,
+      twitterURL: true,
+      githubURL: true,
       bookmarkLists: {
         include: {
           author: {
@@ -27,6 +31,8 @@ async function getUserData(username: User["username"]) {
               image: true,
               username: true,
               bio: true,
+              twitterURL: true,
+              githubURL: true,
             },
           },
           bookmarks: true,
@@ -49,20 +55,54 @@ export default async function UserLists({ params }: UserListsProps) {
   const lists = userData.bookmarkLists
 
   return (
-    <div className="flex h-full flex-col gap-4 p-10">
+    <div className="flex h-full w-full flex-col gap-4 p-10">
       <BackButton />
       <div className="flex flex-col gap-4">
-        <div className="flex gap-2.5 border-b border-gray-6 py-4">
+        <div className="flex gap-2.5 py-4">
           <Avatar name={userData.name!} imageUrl={userData.image!} />
-          <div className="flex w-2/5 flex-col break-words">
-            <h2 className="text-lg font-semibold leading-none">
-              {userData.name}
-            </h2>
-            <p className="text-[13px] text-gray-11">@{userData.username}</p>
-            <p className="mt-2 text-[13px] opacity-80">{userData.bio}</p>
+          <div className="flex justify-between gap-8">
+            <div className="flex flex-col break-words">
+              <h2 className="text-lg font-semibold leading-none">
+                {userData.name}
+              </h2>
+              <p className="text-[13px] text-gray-11">@{userData.username}</p>
+              <p className="mt-2 text-[13px] opacity-80">{userData.bio}</p>
+            </div>
+            <div className="flex items-start gap-2">
+              {userData.twitterURL && (
+                <a
+                  className={clsx(
+                    "cursor-pointer text-gray-11",
+                    "hover:text-gray-12 hover:opacity-80",
+                    "motion-safe:duration-200 motion-safe:ease-productive-standard",
+                  )}
+                  aria-label="twitter profile"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  href={userData.twitterURL}
+                >
+                  <Twitter size={16} />
+                </a>
+              )}
+              {userData.githubURL && (
+                <a
+                  className={clsx(
+                    "cursor-pointer text-gray-11",
+                    "hover:text-gray-12 hover:opacity-80",
+                    "motion-safe:duration-200 motion-safe:ease-productive-standard",
+                  )}
+                  aria-label="github profile"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  href={userData.githubURL}
+                >
+                  <Github size={16} />
+                </a>
+              )}
+            </div>
           </div>
         </div>
-        <div className="columns-md space-y-4 lg:columns-sm">
+        <div className="columns-2xs space-y-4 lg:columns-sm xl:columns-md">
           {lists.map((list) => (
             <ListCard key={list.id} {...list} />
           ))}
