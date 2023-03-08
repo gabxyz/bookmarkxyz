7412b59f7d7fc8d3ec8bb81365da2bbcc0045ea6
gabxyz <gabxyzdev@gmail.com>
2023-03-08 10:54:57 -0300

    add `profile/me` loading ui file and refactor some page styles

---
+++ src/app/profile/me/loading.tsx
@@ -0,0 +1,24 @@
+import { Loader, User } from "lucide-react"
+
+export default function Loading() {
+  return (
+    <div className="flex h-full w-full flex-col gap-10 p-10">
+      <div className="flex items-end justify-between gap-2 border-b border-gray-6 pb-4">
+        <h2 className="flex items-center gap-2 text-xl font-semibold">
+          <div className="flex h-8 w-8 items-center justify-center rounded-full border border-gray-8 bg-gray-4 text-gray-11">
+            <User size={16} />
+          </div>
+          <p>Me</p>
+        </h2>
+        <div className="w-[160px]">
+          <button className="inline-flex w-full max-w-lg items-center justify-center rounded-lg border border-red-6 bg-gray-4 py-1.5 text-sm font-medium text-red-11 shadow-md">
+            Sign out
+          </button>
+        </div>
+      </div>
+      <div className="mt-20 flex justify-center">
+        <Loader size={24} className="animate-spin" />
+      </div>
+    </div>
+  )
+}
--- src/app/profile/me/page.tsx
+++ src/app/profile/me/page.tsx
@@ -2,6 +2,7 @@ import { User } from "@prisma/client"
 import { redirect } from "next/navigation"
 
 import { SignOut } from "@/components/auth-button"
+import Avatar from "@/components/avatar"
 import ProfileForm from "@/components/profile-form"
 import { db } from "@/lib/db"
 import { getCurrentUser } from "@/lib/session"
@@ -24,13 +25,16 @@ export default async function Profile() {
   const userData = await getUser(user.id)
 
   return (
-    <div className="flex h-full flex-col gap-10 p-10">
-      <div className="flex flex-col items-center justify-center gap-8">
-        <p className="text-center font-medium opacity-80">
-          Logged in as <strong>{userData?.username}</strong>
-        </p>
-        <ProfileForm {...userData!} />
+    <div className="flex h-full w-full flex-col gap-4 p-10">
+      <div className="flex items-end justify-between gap-2 border-b border-gray-6 pb-4">
+        <h2 className="flex items-center gap-2 text-xl font-semibold">
+          <Avatar name={userData?.name!} imageUrl={userData?.image!} />
+          <p>Me</p>
+        </h2>
         <SignOut />
+      </div>
+      <div className="flex flex-col items-center justify-center md:mt-10">
+        <ProfileForm {...userData!} />
       </div>
     </div>
   )
