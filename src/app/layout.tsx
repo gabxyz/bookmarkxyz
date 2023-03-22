264eead99b79faba49d4590e41e735f44d8bb448
gabxyz <gabxyzdev@gmail.com>
2023-03-22 11:11:08 -0300

    remove `session-context.tsx` workaround bc it was causing theme issues

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,7 +7,6 @@ import { getServerSession } from "next-auth/next"
 import { Analytics } from "@/components/analytics"
 import Navbar from "@/components/navbar"
 import Providers from "@/components/providers"
-import SessionContext from "@/components/session-context"
 import { authOptions } from "@/pages/api/auth/[...nextauth]"
 
 const fontSans = FontSans({
@@ -47,13 +46,11 @@ export default async function RootLayout({
     <html lang="en" suppressHydrationWarning>
       <body className={clsx("font-sans", fontSans.variable)}>
         <Providers session={session}>
-          <SessionContext>
-            <Navbar />
-            <main className="flex max-h-screen flex-col pt-16 md:ml-16 md:pt-4">
-              {children}
-              <Analytics />
-            </main>
-          </SessionContext>
+          <Navbar />
+          <main className="flex max-h-screen flex-col pt-16 md:ml-16 md:pt-4">
+            {children}
+            <Analytics />
+          </main>
         </Providers>
       </body>
     </html>
