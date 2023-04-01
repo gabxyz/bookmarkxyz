75c4e7001c2f85df6c9a6feed9722694be4bd065
gabxyz <gabxyzdev@gmail.com>
2023-04-01 14:37:20 -0300

    fix text alignment issue

--- src/app/login/loading.tsx
+++ src/app/login/loading.tsx
@@ -3,7 +3,7 @@ import { Loader, User } from "lucide-react"
 export default function Loading() {
   return (
     <div className="flex h-full w-full flex-col gap-10 p-10">
-      <h2 className="flex items-center gap-2 border-b border-gray-6 pb-4 text-xl font-semibold">
+      <h2 className="flex items-end gap-2 border-b border-gray-6 pb-4 text-xl font-semibold">
         <div className="flex h-8 w-8 items-center justify-center rounded-full border border-gray-8 bg-gray-4 text-gray-11">
           <User size={16} />
         </div>
